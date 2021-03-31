45a9ba14c1867c547f2056f6b660584d9952af0f
Leandro Cabral <enigmha@gmail.com>
2021-03-30 23:09:51 -0300

    update

---
+++ src/main/java/com/devsuperior/curso/repositories/OrderRepository.java
@@ -0,0 +1,12 @@
+package com.devsuperior.curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.curso.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+	
+	
+	 
+
+}
